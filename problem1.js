15bf8468ea54784d9151b39d799497ecedf14630
Jeetraj Bisht <jeetraj.bisht.28.1@mountblue.tech>
2024-04-26 20:35:53 +0530

    Creating and deleting json files simultaneously

--- problem1.js
+++ problem1.js
@@ -1,30 +1,54 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-async function rec(directoryPath, count, val) {
-    if (val < count) {
-        const dPath = path.join(directoryPath, `file${val}.json`);
+async function writeFilePromise(filePath, data) {
+    try {
+        await fs.writeFile(filePath, data);
+    } catch (err) {
+        throw new Error(`Failed to write file: ${err.message}`);
+    }
+}
+
+async function unlinkFilePromise(filePath) {
+    try {
+        await fs.unlink(filePath);
+    } catch (err) {
+        throw new Error(`Failed to delete file: ${err.message}`);
+    }
+}
+
+async function createAndDelete(directoryPath, count) {
+    const promises = [];
+    for (let val = 0; val < count; val++) {
+        const filep = `file${val}.json`;
+        const jsonfile = path.join(directoryPath, filep);
+        const jsonData = { random: Math.random() };
+
         try {
-            await fs.writeFile(dPath, 'data');
-            console.log(`file${val} created`);
-            await fs.unlink(dPath);
-            console.log(`file${val} deleted`);
-            await rec(directoryPath, count, val + 1);
-        } catch (err) {
-            console.error(err.message);
+            await writeFilePromise(jsonfile, JSON.stringify(jsonData));
+            console.log('File created', filep);
+        } catch (error) {
+            console.log('Failed to create file:', error.message);
+        }
+
+        try {
+            await unlinkFilePromise(jsonfile);
+            console.log(filep, 'deleted');
+        } catch (error) {
+            throw new Error(`Failed to delete ${filep}`);
         }
     }
+    return promises;
 }
 
 async function createRandomjsonFiles(directoryPath, count) {
     try {
         await fs.mkdir(directoryPath);
         console.log(`Directory created.`);
-        await rec(directoryPath, count, 0);
+        await createAndDelete(directoryPath, count);
+        console.log("All tasks completed successfully!");
     } catch (error) {
         console.error(error.message);
-    } finally {
-        console.log("All tasks completed successfully!");
     }
 }
 
